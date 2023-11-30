e2b2982714bd386c23bc93cdac0ae5c69f4ebd12
Lizeth Marisol Quimi Gómez <lmquimi@espe.edu.ec>
2023-11-30 01:18:37 -0500

    index
    Parte II

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function cambiarEstilo2() {
+    var titulo = document.getElementById("titulo2");
+  
+    // Cambiar alineación del título
+    titulo.style.textAlign = "center";
+  
+    // Cambiar color del título
+    titulo.style.color = "Red";
+  }
+  
